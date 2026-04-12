5fcee5dfbc776c2ae070d256b51f77cc71ba237d
Gorian <github@gorian.io>
2026-04-12 11:49:59 -0700

    fix: rename main window title to NetScope (GUI)

--- cmd/gui-win/main.go
+++ cmd/gui-win/main.go
@@ -81,7 +81,7 @@ func Run(v, target string) {
 		initialTarget = target
 	} else if appConfig.Scan.DefaultTarget != "" {
 		initialTarget = appConfig.Scan.DefaultTarget
-	} else if detected := netinfo.DetectLocalSubnets(); len(detected) > 0 {
+	} else if detected := scan.DetectLocalSubnets(); len(detected) > 0 {
 		initialTarget = detected[0] // best guess; user can click ⟲ to see all
 	} else {
 		initialTarget = "192.168.1.0/24"
@@ -130,7 +130,7 @@ func Run(v, target string) {
 	hwnd, err := createWindowEx(
 		0,
 		"NetScopeWnd",
-		"NetScope",
+		"NetScope (GUI)",
 		WS_OVERLAPPEDWINDOW|WS_CLIPCHILDREN,
 		winX, winY,
 		winW, winH,
